8add85e5d19276005dd0a55b0c5fb1696817ed94
Sara Ali <kalestars@gmail.com>
2024-12-02 17:46:57 +0300

    carousel

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import './App.css'
 import Header from './Components/Header/Header'
+import CarouselEffect from './Components/Carousel Effect/CarouselEffect';
 const App = () => {
   return (
     <div>
       <Header />
+      <CarouselEffect />
     </div>
-  )
+  );
 }
 
 export default App
